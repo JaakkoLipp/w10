1dcbb8b4d21f57213773688a4b3e5fb0f518839f
jaakko lipponen <jaakko.lipponen01@gmail.com>
2022-04-01 14:45:32 +0300

    added comments to file

--- app/src/main/java/com/example/w10/MainActivity.java
+++ app/src/main/java/com/example/w10/MainActivity.java
@@ -17,47 +17,59 @@ public class MainActivity extends AppCompatActivity {
     String sURL = "file:///android_asset/index.html";
     ArrayList<String> prevURL = new ArrayList<String>();
     ArrayList<String> nextURL = new ArrayList<String>();
-    int iB = 0; //used for back iteration
-    int iF = 0;//used for fwd iteration
+    //additional permissions needed for internet in manifest file
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // webview widget configuration & find
         web = findViewById(R.id.wv1);
         urlbar1 = (EditText)findViewById(R.id.urlbar1);
+        // construction
         web.setWebViewClient(new WebViewClient());
+        // allow js to run
         web.getSettings().setJavaScriptEnabled(true);
+        // load main page
         web.loadUrl(sURL);
         //setUrlText();
     }
 
     public void setUrlText(){
+        // shows current site address at url bar 1
         urlbar1.setText(web.getUrl());
     }
 
     public void executeJS(View v){
+        // js
         web.evaluateJavascript("javascript:shoutOut()",null);
     }
 
     public void executeJS2(View v){
+        // js 2
         web.evaluateJavascript("javascript:initialize()",null);
     }
 
     public void goButton(View v){
+        // add site to arrlist of previous sites
         if(prevURL.size()<=9){
         prevURL.add(0,web.getUrl());
         }else{
             prevURL.remove(9);
             prevURL.add(0,web.getUrl());
         }
-        nextURL.clear(); //clears all forward links when leaving history pipe
-
+        //clears all forward links when leaving history pipeline
+        nextURL.clear();
+        // fetch text from address field
         String rawURL = urlbar1.getText().toString();
+
+        // check & fix url format
         if(rawURL.contains("http")){
             web.loadUrl(sURL);
             // correct formatting to not add extra http
             setUrlText();
+            //sets address to text field on site change
         }
         else{
             sURL = "https://"+rawURL+"/";
@@ -68,28 +80,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void refreshButton(View v){
+        // get current url and reload it
         String s = web.getUrl();
         web.loadUrl(s);
 
     }
     public void goBack(View v){
+        //if list empty, prevent crash
         if(prevURL.size()==0) {
             System.out.println("Back array empty");
         }else{
+            // if not empty, go back and rearrange list
             nextURL.add(0, web.getUrl());
             web.loadUrl(prevURL.get(0));
             prevURL.remove(0);
+            //urlbar address show
             setUrlText();
         }
 
     }
     public void goFwd(View v){
+            // same as backButton, if list empty, prevent crash
             if(nextURL.size()==0){
                 System.out.println("Forward array empty");
             }else{
+                //load and rearrange.
                 prevURL.add(0,web.getUrl());
                 web.loadUrl(nextURL.get(0));
                 nextURL.remove(0);
+                // show address
                 setUrlText();
             }
     }
